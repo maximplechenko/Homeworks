38b6593b51570ed09595164412ea08b318163f28
maxim.plechenko <maxim.plechenko@gmail.com>
2018-05-23 00:39:12 +0300

    Homework 4, 1.6

--- Homeworks/Program.cs
+++ Homeworks/Program.cs
@@ -10,6 +10,42 @@ namespace Homeworks
     {
         static void Main(string[] args)
         {
+            //Exercise 4
+            int a = 1010;
+            double b = -22.128;
+            double c = 33.548;
+            string fmt2 = "-##.##;##.##";
+
+            // не получилось найти форматирование, которое удаляло бы знак "-". Пришлось использовать модуль для отрицательного значения переменной b
+            Console.WriteLine("{0,-10:X}{1,-10:##.##}{2,-10:-##.##}", a, Math.Abs(b), c);
+
+
+            /*
+            Task 1.6
+            Написать программу, которая выводит на экран следующую картинку.
+            Дополнительное задание: Использовать только оператор, выводящий один символ: ‘*’ или ‘  ‘.
+            Выполнить программу пошагово.                 
+            */
+
+            Console.WriteLine("{0,6}", '*');
+
+            Console.Write("{0,4}", '*');
+            Console.Write("{0,4}\n", '*');
+
+            Console.Write("{0,2}", '*');
+            Console.Write("{0,8}\n", '*');
+
+            Console.Write('*');
+            Console.Write("{0,10}\n", '*');
+
+            Console.Write("{0,2}", '*');
+            Console.Write("{0,8}\n", '*');
+
+            Console.Write("{0,4}", '*');
+            Console.Write("{0,4}\n", '*');
+
+            Console.WriteLine("{0,6}", '*');
+
         }
     }
 }
